3474f3ae5bd4c7035ca40a27bec2bf1d8438a467
wonkyoung.kim <wwoj0205@gmail.com>
2025-09-23 15:16:46 +0900

    sidemenu 높이값 수정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',       // 정적 HTML export
-  trailingSlash: true,    // GitHub Pages 라우팅 안전하게
+//   output: 'export',       // 정적 HTML export
+  reactStrictMode: true,    // GitHub Pages 라우팅 안전하게
 }
 
 module.exports = nextConfig;
